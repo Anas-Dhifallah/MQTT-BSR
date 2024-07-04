8cacfbccf539d46dd81188e104f47e90d9275b1b
Anas <117837544+Anas-Dhifallah@users.noreply.github.com>
2024-07-04 10:40:46 +0100

    Sends DATA

--- MQTT-receive testing-topic.sh
+++ MQTT-receive testing-topic.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
-# Set username and password 
+# Let the user put in their name and password
 read -p "Username: " username
 read -sp "Password: " password
 echo
 
 # Set MQTT broker address and topic
 broker="localhost"
-topic="testing-topic"
+topic="testing-ground"
 
 # Get the actual port used 
 port=$(grep -E "^listener\s+([0-9]+)" /etc/mosquitto/mosquitto.conf | awk '{print $2}')
 echo "Port used: $port"
 
 # Test connection with authentication
-mosquitto_sub -h $broker -t $topic -v -d -u $username -P $password -p $port | while read -r data; do
-	echo "$data" >> data.txt
-	echo ", " >> data.txt
+mosquitto_sub -h $broker -t $topic -v -d -u $username -P $password -p $port 
+# Copy data to file
+while read -r data; do
+	if [[ $data == test-topic* ]]; then
+		echo "$data" >> data.txt
+		echo ", " >> data.txt
 done
 
 if [ $? -eq 0 ]; then
